577b2b49ba566e6d8d95165687798de1aed2563b
manuel.garc.garc@gmail.com <manuel.garc.garc@gmail.com@df10e183-fe6b-3aa2-35e5-a659256fa3c2>
2014-11-07 02:02:08 +0000

    More advanced progress on sorting

--- branches/SportsStoreTest/SportsStore.Web/Models/ProductListViewModel.cs
+++ branches/SportsStoreTest/SportsStore.Web/Models/ProductListViewModel.cs
@@ -5,7 +5,7 @@ namespace SportsStore.Web.Models
 {
     public class ProductListViewModel
     {
-        public SortingPagingInfo PagingInfo { get; set; }
+        public SortingPagingInfo SortingPagingInfo { get; set; }
         public IEnumerable<Product> Products { get; set; }
         public string CurrentCategory { get; set; }
     }
